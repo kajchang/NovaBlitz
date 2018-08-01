a42b45eb148bfe37827e0ca9fa2799e412869ac0
Kai Chang <kaijchang@gmail.com>
2018-07-31 20:53:25 -0700

    return application/json as Content Type

--- api.py
+++ api.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, send_from_directory, request
+from flask import (Flask, render_template,
+                   send_from_directory, request, make_response, jsonify)
 import glob
 import os
-import json
 
 app = Flask(__name__)
 
@@ -13,21 +13,31 @@ app = Flask(__name__)
 def cardsearch(card_name):
     for file_ in glob.glob('cards/*.json'):
         if os.path.basename(file_.lower()).replace('.json', '') == card_name.lower():
-            return send_from_directory('cards', os.path.basename(file_))
+            resp = make_response(send_from_directory(
+                'cards', os.path.basename(file_)))
+            resp.headers['Content-Type'] = 'application/json'
+
+            return resp
 
     return render_template('404.html', card_name=card_name), 404
 
 
 @app.route('/cards')
 def cards():
-    return json.dumps([json.load(open(file_)) for file_ in glob.glob('cards/*.json') if os.path.basename(
-        file_.lower()).replace('.json', '') in [card.lower() for card in request.args.getlist('cards[]', None)]])
+    resp = make_response(jsonify([json.load(open(file_)) for file_ in glob.glob('cards/*.json') if os.path.basename(
+        file_.lower()).replace('.json', '') in [card.lower() for card in request.args.getlist('cards[]', None)]]))
+    resp.headers['Content-Type'] = 'application/json'
+
+    return resp
 
 
 @app.route('/cardlist')
 def cardlist():
-    return json.dumps([os.path.basename(card).replace(
-        '.json', '') for card in glob.glob('cards/*.json')])
+    resp = make_response(jsonify([os.path.basename(card).replace(
+        '.json', '') for card in glob.glob('cards/*.json')]))
+    resp.headers['Content-Type'] = 'application/json'
+
+    return resp
 
 
 if __name__ == '__main__':
